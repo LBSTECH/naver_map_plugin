0f9a86d08f1121e5c743cf410e253de822c0453d
SugyoIn-LBSTech <50984412+SugyoIn-LBSTech@users.noreply.github.com>
2021-07-23 16:50:04 +0900

    OverlayImage 에서 context 제거

--- lib/src/overlay_image.dart
+++ lib/src/overlay_image.dart
@@ -12,9 +12,7 @@ class OverlayImage {
 
   /// ## [assetName] 이미지 중 [configuration]에 맞는 이미지를 찾아 [OverlayImage]객체를 만든다.
   ///
-  /// [context]를 통해 [ImageConfiguration]을 만들어 이미지를 찾는다.
   /// 이 때 [ImageConfiguration.bundle]은 [PlatformAssetBundle]이 된다.
-  /// [context]가 null일 경우 [ImageConfiguration.locale]과
   /// [ImageConfiguration.textDirection]은 null이 된다.
   ///
   /// - .../image.png  ---> android mdpi(1.0x)와 ios @1x 에서의 기본값
@@ -22,16 +20,9 @@ class OverlayImage {
   /// - .../2.0x/image.png  ---> android xhdpi(2.0x)와 ios @2x 에서 기본
   /// - .../3.0x/image.png  ---> android xxhdpi(3.0x)와 ios @3x 에서 기본
   /// - .../4.0x/image.png  ---> android xxxhdpi(4.0x)에서 기본
-  static Future<OverlayImage> fromAssetImage({
-    required String assetName,
-    required BuildContext context,
-  }) async {
+  static Future<OverlayImage> fromAssetImage({required String assetName}) async {
     final _configuration = ImageConfiguration(
-      devicePixelRatio: MediaQuery.of(context).devicePixelRatio,
-      locale: context == null ? null : Localizations.localeOf(context),
-      textDirection: context == null ? null : Directionality.of(context),
       platform: Platform.isIOS ? TargetPlatform.iOS : TargetPlatform.android,
-      bundle: PlatformAssetBundle(),
     );
     final AssetImage assetImage = AssetImage(assetName);
     final AssetBundleImageKey key = await assetImage.obtainKey(_configuration);
